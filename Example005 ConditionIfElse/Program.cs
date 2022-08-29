3f72686b75ea14af2bef4ccb790fd0584da7d624
BorisSkornyakov <3601124@gmail.com>
2022-08-29 17:39:09 +0500

    Добавили Example005 ConditionIfElse

---
+++ Example005 ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "boris")
+{
+    Console.WriteLine("Здравствуйте, Борис Евгеньевич");
+}
+else 
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
+
+
